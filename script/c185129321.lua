53026664914a67efeabd85a5cf8e8af545595b34
MoonBurst <Moonburstplays@gmail.com>
2021-02-17 11:07:39 -0600

    effect add, might not work

--- script/c185129321.lua
+++ script/c185129321.lua
@@ -24,7 +24,19 @@ function s.initial_effect(c)
 	e3:SetCode(EFFECT_CHANGE_ATTRIBUTE)
 	e3:SetValue(ATTRIBUTE_DARK)
 	c:RegisterEffect(e3)
-	
+	--banish stuff
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetCategory(CATEGORY_REMOVE)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetCountLimit(1)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCondition(s.con)
+	e4:SetCost(s.detatch)
+	e4:SetTarget(s.hdtg)
+	e4:SetOperation(s.activate)
+	c:RegisterEffect(e4)
 	--negate till end phase
 	local e5=Effect.CreateEffect(c)
 	e5:SetDescription(aux.Stringid(id,1))
@@ -42,6 +54,78 @@ end
 function s.xyzcheck(g)
 	return g:IsExists(Card.IsSetCard,1,nil,0x1145)
 end
+--banish stuff
+function s.confilter(c)
+	return c:IsFaceup() and (c:IsType(TYPE_FUSION) or c:IsType(TYPE_SYNCHRO) or c:IsType(TYPE_XYZ) or c:IsType(TYPE_LINK))	
+end
+function s.con(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.confilter,tp,0,LOCATION_MZONE,1,nil)
+end
+function s.detatch(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+    e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+
+
+
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local off=1
+	local ops={}
+	local opval={}
+	if Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_EXTRA,0,1,TYPE_FUSION,tp) and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_ONFIELD,0,1,TYPE_FUSION,tp) then
+		ops[off]=aux.Stringid(id,3)
+		opval[off-1]=1
+		off=off+1
+	end
+	if Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_EXTRA,0,1,TYPE_SYNCHRO,tp) and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_ONFIELD,0,1,TYPE_SYNCHRO,tp) then
+		ops[off]=aux.Stringid(id,4)
+		opval[off-1]=2
+		off=off+1
+	end
+	if Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_EXTRA,0,1,TYPE_XYZ,tp) and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_ONFIELD,0,1,TYPE_XYZ,tp) then
+		ops[off]=aux.Stringid(id,5)
+		opval[off-1]=3
+		off=off+1
+	end
+	if Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_EXTRA,0,1,TYPE_LINK,tp) and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_ONFIELD,0,1,TYPE_LINK,tp) then
+		ops[off]=aux.Stringid(id,6)
+		opval[off-1]=4
+		off=off+1
+	end
+	if off==1 then return end
+	local op=Duel.SelectOption(tp,table.unpack(ops))
+	if opval[op]==1 then --Banish all Fusion
+		local fus=Duel.GetMatchingGroup(Card.IsType,1-tp,0,LOCATION_EXTRA,TYPE_FUSION)
+		Duel.Remove(fus,POS_FACEUP,REASON_EFFECT)
+	elseif opval[op]==2 then --Banish all Synchro
+			local syn=Duel.GetMatchingGroup(Card.IsType,1-tp,0,LOCATION_EXTRA,TYPE_SYNCHRO)
+			Duel.Remove(syn,POS_FACEUP,REASON_EFFECT)
+	elseif opval[op]==3 then --Banish all Xyz
+		local xyz=Duel.GetMatchingGroup(Card.IsType,1-tp,0,LOCATION_EXTRA,TYPE_XYZ)
+		Duel.Remove(xyz,POS_FACEUP,REASON_EFFECT)
+	elseif opval[op]==4 then --Banish all Link
+		local lin=Duel.GetMatchingGroup(Card.IsType,1-tp,0,LOCATION_EXTRA,TYPE_LINK)
+		Duel.Remove(lin,POS_FACEUP,REASON_EFFECT)
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 --negate effect till end phase
 function s.cfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0x1145)
