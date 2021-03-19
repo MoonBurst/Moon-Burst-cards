e1ee37c45a8187cab3c88ede1df5948b365b56ea
Cxpherr <78995420+Cxpherr@users.noreply.github.com>
2021-03-18 23:15:07 -0500

    "Runic Proc" Update

--- script/proc_runic.lua
+++ script/proc_runic.lua
@@ -29,14 +29,34 @@ CANNOT_BE_ALTER_MATERIAL		=0x43000000
 --Procedure - Runic
 
 --c - default
+--cc - counter count (if just 1 counter, put 1)
 --cat - category (if none then put 'nil')
 --sett - settype (if none then put 'nil')
 --setc - setcode
---stopt - settype optional (true/false)
+--stopt - settype optional (TRUE/FALSE)
+--loc - location (if default then put nil)
 --runecon - condition for rune
 --runetg - target for rune (if none then put 'nil')
 --runeop - operation for rune (if none then put 'nil')
-function Runic.AddProcedure(c,cat,sett,setc,stopt,runecon,runetg,runeop)
+function Runic.AddProcedure(c,cat,sett,setc,runecon)
+	--Runic Procedure
+	c:EnableCounterPermit(COUNTER_RUNIC)
+	local e1=Effect.CreateEffect(c)
+	if category then e1:SetCategory(cat+CATEGORY_COUNTER)
+	else e1:SetCategory(CATEGORY_COUNTER) end
+	e1:SetType(sett+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(setc)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(runecon)
+	e1:SetTarget(Runic.Target(c))
+	e1:SetOperation(aux.AND(runeop,Runic.Operation(c)))
+	else e1:SetOperation(Runic.Operation(c))
+	e1:SetValue(EFFECT_RUNE)
+	c:RegisterEffect(e1)
+end
+
+function Runic.AddFunProcedure(c,cc,cat,sett,setc,stopt,loc,runecon,runetg,runeop)
 	--Runic Procedure
 	c:EnableCounterPermit(COUNTER_RUNIC)
 	local e1=Effect.CreateEffect(c)
@@ -46,26 +66,27 @@ function Runic.AddProcedure(c,cat,sett,setc,stopt,runecon,runetg,runeop)
 	else e1:SetType(sett+EFFECT_TYPE_TRIGGER_F) end
 	e1:SetCode(setc)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetRange(LOCATION_MZONE)
+	if not loc then e1:SetRange(LOCATION_MZONE)
+	else e1:SetRange(loc) end
 	e1:SetCondition(runecon)
-	if runetg then e1:SetTarget(aux.AND(runetg,Runic.Target(c)))
-	else e1:SetTarget(Runic.Target(c)) end
-	if runeop then e1:SetOperation(aux.AND(runeop,Runic.Operation(c)))
-	else e1:SetOperation(Runic.Operation(c)) end
+	if runetg then e1:SetTarget(aux.AND(runetg,Runic.Target(c,cc)))
+	else e1:SetTarget(Runic.Target(c,cc)) end
+	if runeop then e1:SetOperation(aux.AND(runeop,Runic.Operation(c,cc)))
+	else e1:SetOperation(Runic.Operation(c,cc)) end
 	e1:SetValue(EFFECT_RUNE)
 	c:RegisterEffect(e1)
 end
 
-function Runic.Target(c)
+function Runic.Target(c,cc)
 	return	function(e,tp,eg,ep,ev,re,r,rp,chk)
 				if chk==0 then return true end
-				Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,COUNTER_RUNIC)
+				Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,cc,0,COUNTER_RUNIC)
 			end
 end
 
-function Runic.Operation(c)
+function Runic.Operation(c,cc)
 	return	function(e,tp,eg,ep,ev,re,r,rp,c)
-				e:GetHandler():AddCounter(COUNTER_RUNIC,1)
+				e:GetHandler():AddCounter(COUNTER_RUNIC,cc)
 			end
 end
 
@@ -82,6 +103,34 @@ function Altar.AddProcedure(c,altarcon)
     e1:SetCode(EFFECT_SPSUMMON_PROC)
     e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
     e1:SetRange(LOCATION_DECK)
+    if altarcon then e1:SetCondition(aux.AND(altarcon,Altar.Condition(c,lv)))
+	else e1:SetCondition(Altar.Condition(c,lv)) end
+    e1:SetTarget(Altar.Target(c,lv))
+    e1:SetOperation(Altar.Operation(c,lv))
+    e1:SetValue(SUMMON_TYPE_ALTAR)
+    c:RegisterEffect(e1)
+	--When Altar Summoned, Place Runic Counters
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_COUNTER)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetLabelObject(e1)
+	e2:SetCondition(Altar.Countercon(c))
+	e2:SetOperation(Altar.Counterop(c))
+	c:RegisterEffect(e2)
+end
+
+function Altar.AddFunProcedure(c,loc,altarcon)
+	--Altar Procedure
+	c:EnableCounterPermit(COUNTER_RUNIC)
+    local lv=c:GetLevel()
+	local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetDescription(1173)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+    if not loc then e1:SetRange(LOCATION_DECK)
+	else e1:SetRange(loc) end
     if altarcon then e1:SetCondition(aux.AND(altarcon,Altar.Condition(c,lv)))
 	else e1:SetCondition(Altar.Condition(c,lv)) end
     e1:SetTarget(Altar.Target(c,lv))
