18073aa7121ffa683eba39b3abb8cbf080ab57bc
MoonBurst <Moonburstplays@gmail.com>
2021-03-24 09:17:24 -0700

    Add files via upload

--- script/C511963031.lua
+++ script/C511963031.lua
@@ -25,28 +25,14 @@ function s.initial_effect(c)
 	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e3:SetOperation(s.atkop)
 	c:RegisterEffect(e3)
-	--banish
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(24696097,2))
-	e4:SetCategory(CATEGORY_REMOVE)
-	e4:SetType(EFFECT_TYPE_QUICK_O)
-	e4:SetCode(EVENT_FREE_CHAIN)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetCountLimit(1)
-	e4:SetCondition(s.bancon)
-	e4:SetTarget(s.bantg)
-	e4:SetOperation(s.banop)
-	c:RegisterEffect(e4)
-	--Return
-	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(24696097,3))
-	e5:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
-	e5:SetCode(EVENT_PHASE+PHASE_END)
-	e5:SetRange(LOCATION_REMOVED)
-	e5:SetCountLimit(1)
-	e5:SetTarget(s.rettg)
-	e5:SetOperation(s.retop)
-	c:RegisterEffect(e5)
+    --atkup
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,0))
+    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e4:SetCode(EVENT_BATTLE_DAMAGE)
+    e4:SetCondition(s.atcon)
+    e4:SetOperation(s.atop)
+    c:RegisterEffect(e4)	
 end
 s.listed_series={0x196}
 function s.filter(c,e,tp)
@@ -69,39 +55,11 @@ end
 function s.aclimit(e,re,tp)
 	return re:IsHasType(EFFECT_TYPE_ACTIVATE)
 end
-function s.bancon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()~=tp and (Duel.IsAbleToEnterBP() or (Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE))
+function s.atcon(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    return Duel.GetAttacker()==c and c:GetFlagEffect(id)==0
+        and c:CanChainAttack()
 end
-function s.bantg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToRemove() end
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,e:GetHandler(),1,0,0)
-end
-function s.banop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and Duel.Remove(c,POS_FACEUP,REASON_EFFECT+REASON_TEMPORARY)>0 then
-		c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
-		if Duel.GetAttacker() and Duel.SelectYesNo(tp,94) then Duel.NegateAttack()
-		else
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
-		e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-		e1:SetRange(LOCATION_REMOVED)
-		e1:SetCountLimit(1)
-		e1:SetOperation(s.negop)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		c:RegisterEffect(e1)
-		end
-	end
-end
-function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:GetFlagEffect(id)>0 end
-end
-function s.retop(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetHandler():IsRelateToEffect(e) then
-		Duel.ReturnToField(e:GetHandler())
-	end
-end
-function s.negop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.NegateAttack()
+function s.atop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.ChainAttack()
 end
