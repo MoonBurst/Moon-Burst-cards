8102677a4ec318a7e5013614d93dca665b5260c5
MoonBurst <Moonburstplays@gmail.com>
2021-03-28 22:27:20 -0500

    Add files via upload

---
+++ script/c511963038.lua
@@ -0,0 +1,59 @@
+--Night Strike
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddEquipProcedure(c)
+	--disable
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_BE_BATTLE_TARGET)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetCondition(s.discon1)
+	e1:SetOperation(s.disop1)
+	c:RegisterEffect(e1)
+	--multi attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_EQUIP)
+	e2:SetCode(EFFECT_EXTRA_ATTACK)
+	e2:SetValue(s.atkval)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x196}
+function s.discon1(e,tp,eg,ep,ev,re,r,rp)
+	local ec=e:GetHandler():GetEquipTarget()
+	return ec and (ec==Duel.GetAttacker() or ec==Duel.GetAttackTarget()) and ec:GetBattleTarget()
+end
+function s.disop1(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=c:GetEquipTarget():GetBattleTarget()
+	c:CreateRelation(tc,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DISABLE)
+	e1:SetProperty(EFFECT_FLAG_OWNER_RELATE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
+	e1:SetCondition(s.discon2)
+	tc:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_CHAIN_SOLVING)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
+	e2:SetOperation(s.disop2)
+	e2:SetLabelObject(tc)
+	c:RegisterEffect(e2)
+end
+function s.discon2(e)
+	return e:GetOwner():IsRelateToCard(e:GetHandler())
+end
+function s.disop2(e,tp,eg,ep,ev,re,r,rp)
+	local loc=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
+	if loc==LOCATION_MZONE and re:GetHandler()==e:GetLabelObject() then
+		Duel.NegateEffect(ev)
+	end
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0x196)
+end
+function s.atkval(e,c)
+  return Duel.GetMatchingGroupCount(s.filter,e:GetHandlerPlayer(),LOCATION_MZONE,0,nil)-1
+end
---
+++ script/c511963039.lua
@@ -0,0 +1,37 @@
+--Night Explosion
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x196}
+function s.cfilter(c)
+	return c:IsSetCard(0x196) and c:IsType(TYPE_MONSTER)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return tp~=Duel.GetTurnPlayer() and Duel.GetAttackTarget()==nil
+		and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,5,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+	if #g~=0 then
+		Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,#g*500)
+	end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.desfilter,tp,0,LOCATION_MZONE,nil)
+	local ct=Duel.Destroy(g,REASON_EFFECT)
+	if ct~=0 then
+		Duel.BreakEffect()
+		Duel.Damage(1-tp,ct*500,REASON_EFFECT)
+	end
+end
